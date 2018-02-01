59f23689db7ed6150d66d2c130f3d28a38b59c18
MarcoEna <marco.ena.91@gmail.com>
2018-02-01 17:15:13 +0100

    the second version of the excercise

---
+++ esercizio.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class java_exercises {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		double v1;
+		int v2 = 0;
+		double media;
+		double sum = 0;
+		System.out.print("waiting for insert");
+		
+		do {v1 = in.nextDouble();
+			System.out.println("Hai inserito: " + v1);
+			if(v1 >= 0) {
+		    sum += v1;
+			v2++;}
+			
+			
+			
+			
+		} while (v1 >= 0);
+		 
+		
+		media = sum / v2;
+		
+		System.out.println("La somma dei numeri inseriti è: " + sum);
+		System.out.println("La media dei numeri inseriti è: " + media);
+		
+		
+
+	}
+
+}
+
